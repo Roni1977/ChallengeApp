fd44eaf1215c8ad9b8348e74667ee27a1707b4b4
Roni <coder.c1977@gmail.com>
2023-04-16 11:14:44 +0200

    Zadanie domowe dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string imie = "Roland";
+string wiek = "pełnoletni";
+string wiek1 = "niepełnoletni";
+bool adult = true;
+int age = 20;
+string resoult1 = "mam na imię";
+string resoult2 = "mam";
+string resoult3 = "i jestem";
+string resoult4 = "i nie jestem";
+char sp = ' ';
+
+
+
+
+if (age >= 18)
+{
+    Console.WriteLine(resoult1 + sp + imie + sp + resoult2 + sp + "lat" + sp + age + sp + resoult3 + sp + wiek);
+}
+else
+{
+    Console.WriteLine(resoult1 + sp + imie + sp + resoult2 + sp + "lat" + sp + age + sp + resoult4 + sp + wiek1);
+}
