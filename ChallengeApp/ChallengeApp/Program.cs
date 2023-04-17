19a424706694973ae0813b094ee3f0124c38b690
Roni <coder.c1977@gmail.com>
2023-04-17 22:13:41 +0200

    Zadanie Dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("zadana liczba to 5422");
 Console.WriteLine(" ");
 int number = 5422;
-string numberInString = number.ToString ();
-char[] letters= numberInString.ToArray ();
-List<char> numbers = new List<char> ();
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+List<char> numbers = new List<char>();
 numbers.Add('0');
 numbers.Add('1');
 numbers.Add('2');
@@ -17,11 +17,11 @@ numbers.Add('9');
 
 foreach (var digital in numbers)
 {
-    int count =0 ;  
-    foreach(var Count in letters) 
+    int count = 0;
+    foreach (var Count in letters)
     {
         if (digital == Count)
-        count++;
-}
+            count++;
+    }
     Console.WriteLine(digital + " ilość znaków w zadanej liczbie: " + count);
 }
