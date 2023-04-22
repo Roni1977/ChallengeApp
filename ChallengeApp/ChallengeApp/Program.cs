c67b350a9d154b9aa23dd4160a3e9a8326757732
Roni <coder.c1977@gmail.com>
2023-04-22 21:35:27 +0200

    Zadanie domowe dzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,27 +1,51 @@
-﻿Console.WriteLine("zadana liczba to 5422");
-Console.WriteLine(" ");
-int number = 5422;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-List<char> numbers = new List<char>();
-numbers.Add('0');
-numbers.Add('1');
-numbers.Add('2');
-numbers.Add('3');
-numbers.Add('4');
-numbers.Add('5');
-numbers.Add('6');
-numbers.Add('7');
-numbers.Add('8');
-numbers.Add('9');
+﻿using ChallengeApp;
+char sp = ' ';
 
-foreach (var digital in numbers)
+Employee employee1 = new Employee("Adam", "Wilk", 22);          //31    
+Employee employee2 = new Employee("Monika", "Żyła", 33);        //18
+Employee employee3 = new Employee("Zuzana", "Drabik", 29);      //36
+Employee employee4 = new Employee("Damian", "Bąk", 44);         //42
+
+employee1.AddScore(9);
+employee1.AddScore(8);
+employee1.AddScore(5);
+employee1.AddScore(2);
+employee1.AddScore(7);
+
+employee2.AddScore(4);
+employee2.AddScore(2);
+employee2.AddScore(3);
+employee2.AddScore(2);
+employee2.AddScore(7);
+
+employee3.AddScore(9);
+employee3.AddScore(9);
+employee3.AddScore(7);
+employee3.AddScore(8);
+employee3.AddScore(3);
+
+employee4.AddScore(9);
+employee4.AddScore(7);
+employee4.AddScore(9);
+employee4.AddScore(8);
+employee4.AddScore(9);
+
+List<Employee> employers = new List<Employee>()
 {
-    int count = 0;
-    foreach (var Count in letters)
+    employee1, employee2, employee3, employee4
+};
+
+var BestResoult = -1;
+Employee? BestEmployee = null;
+
+foreach (var employee in employers)
+{
+    if (employee.Result > BestResoult)
     {
-        if (digital == Count)
-            count++;
+        BestEmployee = employee;
     }
-    Console.WriteLine(digital + " ilość znaków w zadanej liczbie: " + count);
+ 
 }
+Console.WriteLine("Najlepszym pracownikiem jest: " + BestEmployee.Name + " " + BestEmployee.Surname);
+Console.WriteLine("z wynikiem: " + BestEmployee.Result);
+Console.WriteLine("który ma: " + BestEmployee.Age + sp + "lat");
